3a0e5047836d04dfebc6b30c9804324476a399ef
An Vo <thien.an.vo.nguyen@gmail.com>
2018-08-03 10:09:28 +0700

    Fix scope

--- src/config/sneakers.conf.rb
+++ src/config/sneakers.conf.rb
@@ -1,10 +1,10 @@
 workers (ENV["WORKERS"] || 1).to_i
 
 before_fork do
-  ActiveRecord::Base.connection.disconnect!
+  ::ActiveRecord::Base.connection.disconnect!
 end
 
 
 after_fork do
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+  ::ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
